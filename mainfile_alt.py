2636eeb2bb316f0e719110a3d0c11e3daf4210c2
HairBallChunk <michielfirlefyn@hotmail.com>
2021-12-10 21:26:36 +0100

    Implemented DVs and eq7-11

---
+++ mainfile_alt.py
@@ -0,0 +1,123 @@
+#########################################################################################
+#################################### PACKAGES ###########################################
+#########################################################################################
+
+import numpy as np 
+import os
+import pandas as pd
+import time
+from gurobipy import Model,GRB,LinExpr
+import pickle
+from copy import deepcopy
+
+# get distances df
+os.chdir("../data")
+cwd = os.getcwd()
+
+#########################################################################################
+################################### MODEL SETUP #########################################
+#########################################################################################
+
+# get the instance name that is corresponding to the data
+instance_name = 'grid.csv'
+
+distance_name = 'distances.csv'
+daytime_name = 'day_timematrix.csv'
+eveningtime_name = 'evening_timematrix.csv'
+nighttime_name = 'night_timematrix.csv'
+
+distance_frame = pd.read_csv(os.path.join(cwd,distance_name))
+daytime_frame = pd.read_csv(os.path.join(cwd,daytime_name))
+eveningtime_frame = pd.read_csv(os.path.join(cwd,eveningtime_name))
+nighttime_frame = pd.read_csv(os.path.join(cwd,nighttime_name))
+
+# load the data for this instance
+customers = pd.read_csv(os.path.join(cwd,instance_name))
+
+# Keeping track of the start time of the model (for overall performance eval)
+startTime = time.time()
+
+# Initializing an empty Gurobi model
+model = Model()
+
+#########################################################################################
+#################################### VARIABLES ##########################################
+#########################################################################################
+
+# Binary decision variable for every possible route, first consider case for only 1 vehicle, N = 100
+x = {}	# 100x100
+for i in range(len(customers)):
+	for j in range(len(customers)):
+		x[i,j] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY,name="x[%s,%s]"%(i,j))
+
+# List of linehaul customers
+lh = np.where(customers.iloc[:,3].to_numpy() == False)[0]
+# List of backhaul customers
+bh = np.where(customers.iloc[:,3].to_numpy() == True)[0]
+
+# Binary decision variable =1 if customer is served
+# Linhaul customers
+u = {}
+u[0] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY, name="u[0]")		# depot needs to be available
+for i in lh:
+	u[i] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY, name="u[%s]"%(i))
+
+# Backhaul customers
+v = {}
+v[0] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY, name="v[0]")		# depot needs to be available
+for i in bh:
+	v[i] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY, name="v[%s]"%(i))	
+    
+model.update()
+
+#########################################################################################
+################################### CONSTRAINTS #########################################
+#########################################################################################
+
+depot = 43		# after doing np.sqrt((x-x0)**2+(y-y0)**2) for all nodes
+
+# Equation 7
+thisLHS = LinExpr()
+a = 1000		# [kg], amount of product the lh customer demands
+q = 3000		# [kg], cap of the truck
+for i in lh[1:]:
+	thisLHS += a*u[i]
+model.addConstr(lhs=thisLHS, sense=GRB.EQUAL, rhs=q, name='vehicle_cap_lh')
+
+# Equation 8
+thisLHS = LinExpr()
+b = 500			# [kg], amount of product the bh customer offers
+for i in bh[1:]:
+	thisLHS += b*v[i] 
+model.addConstr(lhs=thisLHS, sense=GRB.EQUAL, rhs=q, name='vehicle_cap_bh')
+
+# Equation 9
+thisLHS = LinExpr()
+for j in range(len(customers)):
+	if j in lh[1:]:
+		for i in range(len(customers)):
+			thisLHS += x[i,j] 
+		model.addConstr(lhs=thisLHS, sense=GRB.EQUAL, rhs=u[j], name='flow_arrival_lh_customer_' + str(j))
+	if j in bh:
+		for i in range(len(customers)):
+			thisLHS += x[i,j]
+		model.addConstr(lhs=thisLHS, sense=GRB.EQUAL, rhs=v[j], name='flow_arrival_bh_customer_' + str(j))
+
+# Equation 10
+thisLHS = LinExpr()
+for i in range(len(customers)):
+	if i in lh:
+		for j in range(len(customers)):
+			thisLHS += x[i,j]
+		model.addConstr(lhs=thisLHS, sense=GRB.EQUAL, rhs=u[i], name='flow_departure_lh_customer_' + str(j))
+	if i in bh[1:]:
+		for j in range(len(customers)):
+			thisLHS += x[i,j]
+		model.addConstr(lhs=thisLHS, sense=GRB.EQUAL, rhs=v[i], name='flow_departure_bh_customer_' + str(j))
+
+# Equation 11
+thisLHS = LinExpr()
+for i in lh:
+	for j in bh:
+		thisLHS += x[i,j]
+model.addConstr(lhs=thisLHS, sense=GRB.EQUAL, rhs=1, name='lh_priority')
